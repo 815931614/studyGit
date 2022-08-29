e0c089bcb57973d17601fbb57aaec85fdc78d6dd
815931614 <815931614@qq.com>
2022-08-29 01:32:02 -0700

    笔记

---
+++ 正则表达式/day1/code06.py
@@ -0,0 +1,21 @@
+'''
+Author: 815931614 815931614@qq.com
+Date: 2022-08-29 01:12:34
+LastEditors: 815931614 815931614@qq.com
+LastEditTime: 2022-08-29 01:27:18
+FilePath: /笔记/正则表达式/day1/code06.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+
+'''
+import sys
+
+print(sys.argv)
+
+o = open('./code01.py')
+print(o.__next__())
+print(o.__next__())
+print(o.__next__())
+print(o.__next__())
+print(o.__next__())
+print(o.__next__())
+
